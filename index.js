d383711fff34ff6263e3c93cd78634e22a2f2f9f
Anar Chalkhaasuren <annachak9@gmail.com>
2019-11-29 21:01:45 -0500

    updating add functionality

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+console.log("hello");
 
-    $(".js-shopping-list-form").on('submit', function (event) {
-        let text= $('#shopping-list-entry').val();
-        $('.shopping-list').append($('<li>').append(text));
-        $('#shopping-list-entry').val('');
+    $("#js-shopping-list-form").on('submit', function (event) {
         event.preventDefault();
+
+        let text= $('#shopping-list-entry').val();
+        console.log(text);
+
+        let htmlOutput = `
+        <li>
+        <span class="shopping-item">${text}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>
+        `;
+
+        $('.shopping-list').append(htmlOutput);
+        $('#shopping-list-entry').val('');
+       
     });
 //     $('#shopping-list').on('click','li', function (event) {
 //         $({this: check , this:remove});
